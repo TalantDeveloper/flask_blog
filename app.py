45d82f4291b3a70b6e490f641e4da00c66fff2d3
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2023-08-14 19:11:46 +0500

    14.08.2023
    19:11

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '762ea6792c314d4af7544fc40f32bf79'
 
 posts = [
     {
@@ -28,6 +31,20 @@ def hello_world():  # put application's code here
 def about():
     return render_template('about.html', title="About")
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Login", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
